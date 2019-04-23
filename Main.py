7df9c069179aac83e4a335a3902f36bf9b6a632b
MissLibertys <46232173+MissLibertys@users.noreply.github.com>
2019-04-22 17:34:50 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -118,7 +118,7 @@ async def _play(ctx, *, name):
 	player.start()
 	embed = discord.Embed(description=" ")
 	embed.add_field(name="Now Playing", value=title)
-	embed.add_field(name="Youtube video", value=f"[Link](https://youtube.com + title)")
+	embed.add_field(name="Youtube Video Link", value=f"[Link](https://youtube.com)")
 	await client.say(embed=embed)
 	
 @client.command(pass_context=True)
