e1de141d1fb1342261c3ac2270bd5a18501fb3dc
MissLibertys <46232173+MissLibertys@users.noreply.github.com>
2019-05-22 20:34:41 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,10 +10,6 @@ async def on_ready():
   print("Logged in as")
   print("User Name", bot.user.name)
   print("User ID", bot.user.id)
-  
-@bot.command()
-async def test(ctx):
-  await ctx.send("test")
 
 @bot.command()
 async def ping(ctx):
