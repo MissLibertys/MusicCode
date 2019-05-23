1f72eba537e72c5ebf616f0bc1efdc292319e23b
MissLibertys <46232173+MissLibertys@users.noreply.github.com>
2019-05-22 19:58:59 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -13,5 +13,12 @@ async def on_ready():
 @bot.command()
 async def test(ctx):
   await ctx.send("test")
+
+@bot.command()
+async def ping(ctx):
+  pingtime = time.time()
+  pingms = await ctx.send("Pinging...")
+  ping = (time.time() - pingtime) * 1000
+  await ctx.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
   
 bot.run(os.environ["BOT_TOKEN"])
