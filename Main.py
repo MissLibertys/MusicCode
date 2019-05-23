571db7e40f7c71edf9a25340b7ad815f481e07d6
MissLibertys <46232173+MissLibertys@users.noreply.github.com>
2019-05-22 20:29:58 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -17,9 +17,160 @@ async def test(ctx):
 
 @bot.command()
 async def ping(ctx):
-  pingtime = time.time()
-  pingms = await ctx.send("Pinging...")
-  ping = (time.time() - pingtime) * 1000
-  await ctx.edit_message(pingms, content="Pong! :ping_pong: ping time is `%dms`" % ping)
+  await ctx.send("Pong! :ping_pong: ping time is `116ms`")
+  
+@bot.command(name="join", no_pm=True)
+async def _join(ctx):
+	user = ctx.message.author
+	channel = ctx.message.author.voice.voice_channel
+	await client.join_voice_channel(channel)
+	embed = discord.Embed(colour=user.colour)
+	embed.add_field(name="Successfully connected to voice channel:", value=channel)
+	await ctx.send(embed=embed)
+	
+@bot.command(name="leave", no_pm=True)
+async def _leave(ctx):
+	user = ctx.message.author
+	server = ctx.message.server
+	channel = ctx.message.author.voice.voice_channel
+	voice_client = client.voice_client_in(server)
+	await voice_client.disconnect()
+	embed = discord.Embed(colour=user.colour)
+	embed.add_field(name="Successfully disconnected from:", value=channel)
+	await ctx.send(embed=embed)
+
+@bot.command()
+async def pause(ctx):
+	user = ctx.message.author
+	id = ctx.message.server.id
+	players[id].pause()
+	embed = discord.Embed(colour=user.colour)
+	embed.add_field(name="Player Paused", value=f"Requested by {ctx.message.author.name}")
+	await ctx.send(embed=embed)
+
+@bot.command()
+async def skip(ctx):
+	user = ctx.message.author
+	id = ctx.message.server.id
+	players[id].stop()
+	embed = discord.Embed(colour=user.colour)
+	embed.add_field(name="Player Skipped", value=f"Requested by {ctx.message.author.name}")
+	await ctx.send(embed=embed)
+	
+@bot.command(name="play")
+async def _play(ctx, *, name):
+	author = ctx.message.author
+	name = ctx.message.content.replace("m.play ", '')
+	fullcontent = ('http://www.youtube.com/results?search_query=' + name)
+	text = requests.get(fullcontent).text
+	soup = bs4.BeautifulSoup(text, 'html.parser')
+	img = soup.find_all('img')
+	div = [ d for d in soup.find_all('div') if d.has_attr('class') and 'yt-lockup-dismissable' in d['class']]
+	a = [ x for x in div[0].find_all('a') if x.has_attr('title') ]
+	title = (a[0]['title'])
+	a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
+	url = ('http://www.youtube.com'+a0['href'])
+	server = ctx.message.server
+	voice_client = client.voice_client_in(server)
+	player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+	players[server.id] = player
+	print("User: {} From Server: {} is playing {}".format(author, server, title))
+	player.start()
+	embed = discord.Embed(description=" ")
+	embed.add_field(name="Now Playing", value=title)
+	await ctx.send(embed=embed)
+	
+@bot.command()
+async def queue(ctx, *, name):
+	name = ctx.message.content.replace("m.queue ", '')
+	fullcontent = ('http://www.youtube.com/results?search_query=' + name)
+	text = requests.get(fullcontent).text
+	soup = bs4.BeautifulSoup(text, 'html.parser')
+	img = soup.find_all('img')
+	div = [ d for d in soup.find_all('div') if d.has_attr('class') and 'yt-lockup-dismissable' in d['class']]
+	a = [ x for x in div[0].find_all('a') if x.has_attr('title') ]
+	title = (a[0]['title'])
+	a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
+	url = ('http://www.youtube.com'+a0['href'])
+	server = ctx.message.server
+	voice_client = client.voice_client_in(server)
+	player = await voice_client.create_ytdl_player(url, after=lambda: check_queue(server.id))
+	
+	if server.id in queues:
+		queues[server.id].append(player)
+	else:
+		queues[server.id] = [player]
+	embed = discord.Embed(description=" ")
+	embed.add_field(name="Video queued", value=title)
+	await ctx.send(embed=embed)
+
+@bot.command()
+async def resume(ctx):
+	user = ctx.message.author
+	id = ctx.message.server.id
+	players[id].resume()
+	embed = discord.Embed(colour=user.colour)
+	embed.add_field(name="Player Resumed", value=f"Requested by {ctx.message.author.name}")
+	await ctx.send(embed=embed)
+	
+@bot.command(name="stats")
+async def _stats(ctx):
+	servers = list(client.servers)
+	current_time = time.time()
+	difference = int(round(current_time - start_time))
+	text = str(datetime.timedelta(seconds=difference))
+	embed = discord.Embed(title="Servers:", description=f"{str(len(servers))}", color=0xFFFF)
+	embed.add_field(name="Users:", value=f"{str(len(set(client.get_all_members())))}")
+	embed.add_field(name="Uptime:", value=f"{text}")
+	await ctx.send(embed=embed)
+	
+@bot.command()
+async def support(ctx):
+	user = ctx.message.author
+	servers = list(client.servers)
+	embed = discord.Embed(color=user.colour)
+	embed.add_field(name="Support server", value=f"[Link](https://discord.gg/ccAuKgV)")
+	embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/489033991769423873/5215f2354e333ef5ca21124d45f70efd.png?size=1024")
+	await ctx.send(embed=embed)
+	
+@bot.command()
+async def invite(ctx):
+	user = ctx.message.author
+	embed = discord.Embed(color=user.colour)
+	embed.add_field(name="Bot Link", value=f"[Link](https://discordapp.com/api/oauth2/authorize?client_id=489033991769423873&permissions=36785152&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Foauth2%2Fauthorize%3Fclient_id%3D489033991769423873%26permissions%3D36784128%26redirect_uri%3Dhttps%253A%252F%252Fdiscordapp.com%252Fapi%252Foauth2%252Fauthorize%253Fclient_id&scope=bot)")
+	await ctx.send(embed=embed)
+	
+@bot.command()
+async def test(ctx):
+	user = ctx.message.author
+	embed = discord.Embed(colour=user.colour)
+	embed.add_field(name="Music commands:", value="m.play | m.join | m.leave | m.pause | m.resume | m.skip | m.queue", inline=True)
+	embed.add_field(name="Credits:", value="m.credits")
+	embed.add_field(name="Other commands:", value="m.ping | m.support | m.stats | m.invite")
+	await ctx.send(embed=embed)
+
+@bot.command(no_pm=True)
+async def credits(ctx):
+	"""credits who helped me"""
+	await ctx.send('iHoverZz#2321 helped me with this music bot')
+	
+def user_is_me(ctx):
+	return ctx.message.author.id == "277983178914922497"
+
+@bot.command(name='eval')
+@commands.check(user_is_me)
+async def _eval(ctx, *, command):
+	res = eval(command)
+	if inspect.isawaitable(res):
+		await ctx.send(await res)
+	else:
+		await client.delete_message(ctx.message)
+		await ctx.send(res)
+        
+@_eval.error
+async def eval_error(error, ctx):
+	if isinstance(error, discord.ext.commands.errors.CheckFailure):
+		text = "Sorry {}, You can't use this command only the bot owner can do this.".format(ctx.message.author.mention)
+		await ctx.send(ctx.message.channel, text)
   
 bot.run(os.environ["BOT_TOKEN"])
