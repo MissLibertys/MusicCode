fd9c0004d15764a103e6b582dfd8c15a6f70618c
MissLibertys <46232173+MissLibertys@users.noreply.github.com>
2019-05-22 19:18:05 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,17 @@
+import discord
+from discord.ext import commands
+import os
 
+bot = commands.Bot(command_prefix = "m.")
+
+@bot.event
+async def on_ready():
+  print("Logged in as")
+  print("User Name", bot.user.name)
+  print("User ID", bot.user.id)
+  
+@bot.command()
+async def test():
+  await bot.say("test")
+  
+bot.run(os.environ["BOT_TOKEN"])
